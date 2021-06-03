d3d6398d4aad949096f754693b69d3f72b752487
qcalmel <quentin.calmel@gmail.com>
2021-06-03 20:06:05 +0200

    add transition when changing page

--- src/App.js
+++ src/App.js
@@ -7,20 +7,40 @@ import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import {CSSTransition} from "react-transition-group";
+import "../src/styles/pageTransition.css"
 
 function App() {
+    const routes = [
+        {path: ['/','/skills'], name: 'COMPETENCES', Component: Skills},
+        {path: '/about', name: 'PRESENTATION', Component: About},
+        {path: '/projects', name: 'PROJETS', Component: Projects},
+        {path: '/contact', name: 'CONTACT', Component: Contact}
+    ]
     return (
         <BrowserRouter>
             <div className="App">
                 <Header/>
-                <Menu/>
+                <Menu routes={routes}/>
                 <div className="content">
-                    <Switch>
-                        <Route exact path={["/", "/competences"]} component={Skills}/>
-                        <Route exact path={"/moi"} component={About}/>
-                        <Route exact path={"/projets"} component={Projects}/>
-                        <Route exact path={"/contact"} component={Contact}/>
-                    </Switch>
+                    {
+                        routes.map(({path, Component}) => (
+                            <Route exact path={path}>
+                                {({match}) => (
+                                    <CSSTransition
+                                        in={match != null}
+                                        timeout={300}
+                                        classNames="page"
+                                        unmountOnExit
+                                    >
+                                        <div className="page">
+                                            <Component/>
+                                        </div>
+                                    </CSSTransition>
+                                )}
+                            </Route>
+                        ))
+                    }
                 </div>
                 <Footer/>
             </div>
