b9ae96b89842adbf0d7f97b9bebc69da7c6490ec
qcalmel <quentin.calmel@gmail.com>
2021-06-03 12:05:09 +0200

    Create About component

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,27 @@
+import "../styles/About.css"
 const About = () => {
     return (
         <div className="About">
+            <blockquote>Mais pourquoi n'ai-je pas fait ce métier plus tôt ?</blockquote>
+            <p>Moi c'est Quentin, j'ai 30 ans et j'ai récemment rejoint le monde merveilleux du développement après
+                une reconversion professionnelle.</p>
 
+            <p>Cette question a longtemps résonné dans ma tête depuis mon changement de métier.<br />
+                C'est vrai, depuis mon plus jeune âge, j'ai toujours été accro à l'informatique, je ne compte plus
+                le nombre d'ordinateurs que j'ai montés, bidouillés, réparés ou le nombre d'heures passées à
+                apprendre tout un tas de logiciels.</p>
+
+            <p>Et aujourd'hui, j'ai trouvé ma voie.</p>
+
+            <p>A chaque nouveau projet, ce sont de nouvelles découvertes qui permettent de m'améliorer et
+                d'étancher ma soif d'en apprendre toujours plus.</p>
+
+            <p>Dans ce métier, je retrouve plein d'aspects que j'apprécie au quotidien: la curiosité, la rigueur,
+                l'entraide, la polyvalence ou bien les nombreuses possibilités dans la résolution d'un problème.</p>
+
+            <p>Sinon que dire de plus à propos de moi ? Je pense que vous l'aurez remarqué, le Japon est mon pays de
+                coeur, j'adore monter des maquettes de Gundam (robots japonais) et les road-trip à vélo ne me font
+                pas peur.</p>
         </div>
     )
 }
