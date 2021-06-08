095aec2f9e6b24b1244e22cc757bba1b8459154a
qcalmel <quentin.calmel@gmail.com>
2021-06-08 10:50:08 +0200

    Create contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,33 @@
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faEnvelope, faPhone} from "@fortawesome/free-solid-svg-icons";
+import {faLinkedin} from "@fortawesome/free-brands-svg-icons";
+
+import "../styles/Contact.css"
+
 const Contact = () => {
     return (
         <div className="Contact">
-
+            <p className="hidden">
+                30 ans
+            </p>
+            <p>
+                <a target="_blank" href="mailto:contact@quentincalmel.fr">
+                    <FontAwesomeIcon icon={faEnvelope}/>
+                    contact@quentincalmel.fr
+                </a>
+            </p>
+            <p>
+                <a href="tel:0645499986">
+                    <FontAwesomeIcon icon={faPhone}/>
+                    06 45 49 99 86
+                </a>
+            </p>
+            <p>
+                <a target="_blank" href="https://www.linkedin.com/in/quentin-calmel-5b29661b5/">
+                    <FontAwesomeIcon icon={faLinkedin}/>
+                    Mon profil
+                </a>
+            </p>
         </div>
     )
 }
