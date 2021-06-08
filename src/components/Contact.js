46edaf3498db4bc0219aaff6b4f3e92e570b83a4
qcalmel <quentin.calmel@gmail.com>
2021-06-08 20:43:07 +0200

    Add right margin to icons

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -12,20 +12,20 @@ const Contact = () => {
             </p>
             <p>
                 <a target="_blank" href="mailto:contact@quentincalmel.fr">
-                    <FontAwesomeIcon icon={faEnvelope}/>
+                    <FontAwesomeIcon className="Contact__icon" icon={faEnvelope} />
                     contact@quentincalmel.fr
                 </a>
             </p>
             <p>
                 <a href="tel:0645499986">
-                    <FontAwesomeIcon icon={faPhone}/>
+                    <FontAwesomeIcon className="Contact__icon" icon={faPhone}/>
                     06 45 49 99 86
                 </a>
             </p>
             <p>
                 <a target="_blank" href="https://www.linkedin.com/in/quentin-calmel-5b29661b5/">
-                    <FontAwesomeIcon icon={faLinkedin}/>
-                    Mon profil
+                    <FontAwesomeIcon className="Contact__icon" icon={faLinkedin}/>
+                    Mon profil LinkedIn
                 </a>
             </p>
         </div>
