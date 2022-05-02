fb7edddf8605d9fd8a24fbd497a0ab6771223e7c
qcalmel <contact@quentincalmel.fr>
2022-05-02 10:45:00 +0200

    add photo in about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,17 @@
 import "../styles/About.css"
+import photo from "../assets/me-1.png"
+
 const About = () => {
     return (
         <div className="About">
+            <div className="About__photo-container">
+                <div className="About__photo"><img src={photo}/></div>
+            </div>
             <blockquote>Mais pourquoi n'ai-je pas fait ce métier plus tôt ?</blockquote>
             <p>Moi c'est Quentin, j'ai 30 ans et j'ai récemment rejoint le monde merveilleux du développement après
                 une reconversion professionnelle.</p>
 
-            <p>Cette question a longtemps résonné dans ma tête depuis mon changement de métier.<br />
+            <p>Cette question a longtemps résonné dans ma tête depuis mon changement de métier.<br/>
                 C'est vrai, depuis mon plus jeune âge, j'ai toujours été accro à l'informatique, je ne compte plus
                 le nombre d'ordinateurs que j'ai montés, bidouillés, réparés ou le nombre d'heures passées à
                 apprendre tout un tas de logiciels.</p>
@@ -19,8 +24,8 @@ const About = () => {
             <p>Dans ce métier, je retrouve plein d'aspects que j'apprécie au quotidien: la curiosité, la rigueur,
                 l'entraide, la polyvalence ou bien les nombreuses possibilités dans la résolution d'un problème.</p>
 
-            <p>Sinon que dire de plus à propos de moi ? Je pense que vous l'aurez remarqué, le Japon est mon pays de
-                coeur, j'adore monter des maquettes de Gundam (robots japonais) et les road-trip à vélo ne me font
+            <p>Sinon que dire de plus à propos de moi ?le Japon est mon pays de
+                coeur, j'adore monter des maquettes et les road-trip à vélo ne me font
                 pas peur.</p>
         </div>
     )
