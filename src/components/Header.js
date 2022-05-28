2b36f3e48b8e147e842c6b24610136b857695c63
qcalmel <contact@quentincalmel.fr>
2022-05-28 17:56:48 +0200

    add Menu to Header component & add function to reduce header when scrolling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,32 +1,51 @@
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faBars} from "@fortawesome/free-solid-svg-icons";
-import {ReactComponent as Logo} from "../assets/portfolio-logo.svg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { ReactComponent as Logo } from "../assets/portfolio-logo.svg";
 
-import "../styles/Header.css"
-const Header = () => {
-    const showMenu = () => {
-        const menu = document.querySelector('.Menu')
-        menu.classList.remove('Menu--hidden')
-        document.querySelector('.Menu__nav').classList.add('Menu__nav--show')
-    }
-    return (
-        <div className="Header">
-            <div className="menu__btn" onClick={showMenu}>
-                <FontAwesomeIcon className="menu__icon" icon={faBars}/>
-            </div>
+import "../styles/Header.css";
+import Menu from "./Menu";
+import { useEffect, useState } from "react";
+const Header = ({ routes }) => {
+  const [isMinimized, setIsMinimized] = useState(false);
+  const checkScrollYPos = () => {
+    const minScrollY = isMinimized ? 0 : 248;
+    if (window.scrollY > minScrollY) setIsMinimized(true);
+    else setIsMinimized(false);
+  };
+  useEffect(() => {
+    const watchScroll = () => {
+      window.addEventListener("scroll", checkScrollYPos);
+    };
+    watchScroll();
+    return () => {
+      window.removeEventListener("scroll", checkScrollYPos);
+    };
+  });
 
-
-            <div>
-                <span className="Header__name">Quentin CALMEL</span>
-                <span className="Header__job">Un Développeur Web</span>
-                <span className="Header__lede">Curieux et passionné</span>
-            </div>
-            <div className="Header__picture-container">
-                <Logo/>
-            </div>
+  const showMenu = () => {
+    const menu = document.querySelector(".Menu");
+    menu.classList.remove("Menu--hidden");
+    document.querySelector(".Menu__nav").classList.add("Menu__nav--show");
+  };
+  return (
+    <>
+      <div className={"Header" + (isMinimized ? " Header--hidden" : "")}>
+        <div className="menu__btn" onClick={showMenu}>
+          <FontAwesomeIcon className="menu__icon" icon={faBars} />
         </div>
-)
+        <div>
+          <span className="Header__name">Quentin CALMEL</span>
+          <span className="Header__job">Un Développeur Web</span>
+          <span className="Header__lede">Curieux et passionné</span>
+        </div>
+        <div className="Header__picture-container">
+          <Logo />
+        </div>
+      </div>
+      <Menu routes={routes} isMinimized={isMinimized} />
+      <div className={isMinimized ? "margin" : ""} />
+    </>
+  );
+};
 
-}
-
-export default Header
+export default Header;
