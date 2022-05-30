f2bf908bb24f47532f26bbc4d65c541daeac54c6
qcalmel <contact@quentincalmel.fr>
2022-05-30 17:41:34 +0200

    create Career component

---
+++ src/components/Career.js
@@ -0,0 +1,11 @@
+import "../styles/Career.css";
+const Career = () => {
+  return (
+    <div id="career" className="Carrer Section">
+      <h1 className="Section__title">Experiences</h1>
+      <h1 className="Section__title">Formations</h1>
+    </div>
+  );
+};
+
+export default Career;
