9ba1565ee302c3cb08a77b87f656ac2a586335fc
qcalmel <contact@quentincalmel.fr>
2022-05-31 10:10:09 +0200

    change text size of About component

--- src/components/About.js
+++ src/components/About.js
@@ -1,34 +1,52 @@
-import "../styles/About.css"
-import photo from "../assets/me-1.png"
+import "../styles/About.css";
+import photo from "../assets/me-1.png";
 
 const About = () => {
-    return (
-        <div className="About">
-            <div className="About__photo-container">
-                <div className="About__photo"><img src={photo}/></div>
-            </div>
-            <blockquote>Mais pourquoi n'ai-je pas fait ce métier plus tôt ?</blockquote>
-            <p>Moi c'est Quentin, j'ai 30 ans et j'ai récemment rejoint le monde merveilleux du développement après
-                une reconversion professionnelle.</p>
-
-            <p>Cette question a longtemps résonné dans ma tête depuis mon changement de métier.<br/>
-                C'est vrai, depuis mon plus jeune âge, j'ai toujours été accro à l'informatique, je ne compte plus
-                le nombre d'ordinateurs que j'ai montés, bidouillés, réparés ou le nombre d'heures passées à
-                apprendre tout un tas de logiciels.</p>
-
-            <p>Et aujourd'hui, j'ai trouvé ma voie.</p>
-
-            <p>A chaque nouveau projet, ce sont de nouvelles découvertes qui permettent de m'améliorer et
-                d'étancher ma soif d'en apprendre toujours plus.</p>
-
-            <p>Dans ce métier, je retrouve plein d'aspects que j'apprécie au quotidien: la curiosité, la rigueur,
-                l'entraide, la polyvalence ou bien les nombreuses possibilités dans la résolution d'un problème.</p>
-
-            <p>Sinon que dire de plus à propos de moi ?le Japon est mon pays de
-                coeur, j'adore monter des maquettes et les road-trip à vélo ne me font
-                pas peur.</p>
+  return (
+    <div className="About Section">
+      <div className="About__photo-container">
+        <div className="About__photo">
+          <img src={photo} alt="Quentin" />
         </div>
-    )
-}
+      </div>
+      <blockquote>
+        Mais pourquoi n'ai-je pas fait ce métier plus tôt ?
+      </blockquote>
+      <p>
+        Moi c'est Quentin, j'ai 30 ans et j'ai récemment rejoint le monde
+        merveilleux du développement après une reconversion professionnelle.
+      </p>
 
-export default About
+      <p>
+        Cette question a longtemps résonné dans ma tête depuis mon changement de
+        métier.
+        <br />
+        C'est vrai, depuis mon plus jeune âge, j'ai toujours été accro à
+        l'informatique, je ne compte plus le nombre d'ordinateurs que j'ai
+        montés, bidouillés, réparés ou le nombre d'heures passées à apprendre
+        tout un tas de logiciels.
+      </p>
+
+      <p>Et aujourd'hui, j'ai trouvé ma voie.</p>
+
+      <p>
+        A chaque nouveau projet, ce sont de nouvelles découvertes qui permettent
+        de m'améliorer et d'étancher ma soif d'en apprendre toujours plus.
+      </p>
+
+      <p>
+        Dans ce métier, je retrouve plein d'aspects que j'apprécie au quotidien:
+        la curiosité, la rigueur, l'entraide, la polyvalence ou bien les
+        nombreuses possibilités dans la résolution d'un problème.
+      </p>
+
+      <p>
+        Sinon que dire de plus à propos de moi ?le Japon est mon pays de coeur,
+        j'adore monter des maquettes et les road-trip à vélo ne me font pas
+        peur.
+      </p>
+    </div>
+  );
+};
+
+export default About;
